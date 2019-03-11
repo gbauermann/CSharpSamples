0d1340ba771202e66d5b0c45d2af02f2398bf992
gabriel.bauermann <gabriel.bauermann@gmail.com>
2019-03-11 16:34:28 -0300

    Delegates, Funcs e Alerts

---
+++ DelegatesConsoleUI/Models.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DelegatesConsoleUI
+{
+    public class ProductModel
+    {
+        public string ItemName { get; set; }
+        public decimal Price { get; set; }
+       
+    }
+
+    public class ShoppingCartModel
+    {
+        public delegate void MentionDiscount(decimal subtotal);
+
+        public List<ProductModel> Items { get; set; }
+
+        public ShoppingCartModel()
+        {
+            Items = new List<ProductModel>();
+        }
+
+        public decimal GenerateTotal(MentionDiscount mentionSubtotal,
+            Func<List<ProductModel>, decimal, decimal> calculateDiscountedTotal,
+            Action<string> tellUserWeAreDicounting)
+        {
+            var subtotal = Items.Sum(i => i.Price);
+
+            mentionSubtotal(subtotal);
+
+            tellUserWeAreDicounting("Aplicando desconto");
+
+            return calculateDiscountedTotal(Items, subtotal);
+        }
+    }
+}
---
+++ DelegatesConsoleUI/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesConsoleUI
+{
+    class Program
+    {
+        static ShoppingCartModel cart = new ShoppingCartModel();
+
+        static void Main(string[] args)
+        {
+            PopulateCart();
+
+            Console.WriteLine($"Total do carrinho é {cart.GenerateTotal(subtotalAlert, calculateLeveledDiscount, alertUser):C2}");
+            Console.ReadKey();
+
+        }
+
+        private static void PopulateCart()
+        {
+            cart.Items.Add(new ProductModel() { ItemName = "Chocolate", Price = 3.5m });
+            cart.Items.Add(new ProductModel() { ItemName = "Pringles", Price = 4.75m });
+            cart.Items.Add(new ProductModel() { ItemName = "Cerveja", Price = 2.5m });
+            cart.Items.Add(new ProductModel() { ItemName = "Cookie", Price = 3.25m });
+        }
+
+        private static void subtotalAlert(decimal subtotal)
+        {
+            Console.WriteLine($"O subtotal é {subtotal:C2}");
+        }
+
+        private static void alertUser(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private static decimal calculateLeveledDiscount(List<ProductModel> items, decimal subtotal)
+        {
+
+            if (subtotal > 100)
+                return subtotal * 0.8m;
+            else if (subtotal > 50)
+                return subtotal * 0.85m;
+            else if (subtotal > 10)
+                return subtotal * 0.9m;
+            else
+                return subtotal;
+        }
+    }
+}
